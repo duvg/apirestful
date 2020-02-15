71b26de333a885ce9bf238e771c10874a55d20df
duviel <duviel7@gmail.com>
2020-02-15 01:09:41 -0500

    Manejo de excepciones para dar respuesta con mensajes peronalizados en formato json

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,19 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that should not be reported.
      *
@@ -44,7 +51,62 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+
+        // Handle validations exceptions
+        if ($exception instanceof ValidationException) 
+        {
+            $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        // Handle model not found exception
+        if ($exception instanceof ModelNotFoundException) 
+        {
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("No existe ninguna instancia de $model con el id especificado", 404);
+        }
+
+        // Handle authentication exceptions
+        if ($exception instanceof AuthenticationException) 
+        {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        // Handle authorization exceptions
+        if ($exception instanceof AuthorizationException) 
+        {
+            return $this->errorResponse('No tienes permisos para acceder a este recurso', 403);
+        }
+
+        // Handle not found http exception
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('No se encontro la URL especificada', 404);
+        }
+
+        // Handle method not allowed
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('El metodo especificado en la petición no es valido', 405);
+        }
+
+        // Handle any exception hhttp
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        // Handle integrity relationship exception
+        if ($exception instanceof QueryException) {
+            $code = $exception->errorInfo[1];
+            if ($code == 1451) {
+                 return $this->errorResponse('No es posible eliminar de forma permanete el recurso porque esta relacionado con otro', 409);
+            }
+            
+        }
+
+        // Handle unexpected exceptions
+
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+        return $this->errorResponse('Ups! algo salio mal, intenta más tarde', 500);
     }
 
     /**
@@ -55,11 +117,21 @@ class Handler extends ExceptionHandler
      * @return \Illuminate\Http\Response
      */
     protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
+    {   
+        return response()->json(['error' => 'Unauthenticated.'], 401);
+        
+    }
 
-        return redirect()->guest(route('login'));
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $exception, $request)
+    {
+        $errors = $exception->validator->errors()->getMessages();
+        return $this->errorResponse($errors, 200);
     }
 }
