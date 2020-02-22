667f1eb94e519ae9ad6450fdc6ab021ac23ec49e
duviel <duviel7@gmail.com>
2020-02-22 11:39:44 -0500

    Configuración e implementación del cache

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -35,6 +36,7 @@ trait ApiResponser
 		$collection = $this->sortData($collection, $transformer);
 		$collection = $this->paginate($collection);
 		$collection = $this->transformData($collection, $transformer);
+		$collection = $this->cacheResponse($collection);
 
 		return $this->successResponse($collection, $code);
 	}
@@ -116,6 +118,22 @@ trait ApiResponser
 		$transformation = fractal($data, new $transformer);
 
 		return $transformation->toArray();
+	}
+
+	protected function cacheResponse($data)
+	{
+		$url = request()->url();
+		$queryParams = request()->query();
+
+		ksort($queryParams);
+
+		$queryString = http_build_query($queryParams);
+
+		$fullUrl = "{$url}?{$queryString}";
+
+		return Cache::remember($fullUrl, 30/60, function() use ($data) {
+			return $data;
+		});
 	}
 
 
